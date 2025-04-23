68438037c3078a0b55fbd024b0a8fac79f60899d
kaisukaisu <kaisu.konttinen@hotmail.com>
2025-04-23 15:52:13 +0300

    commit jee

--- src/main/java/com/example/miniprojekti/Yleiskalenteri.java
+++ src/main/java/com/example/miniprojekti/Yleiskalenteri.java
@@ -1,10 +1,15 @@
 package com.example.miniprojekti;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 import com.calendarfx.view.CalendarView;
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
+import com.calendarfx.model.Calendar.Style;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 
 public class Yleiskalenteri extends Application {
@@ -14,9 +19,11 @@ public class Yleiskalenteri extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
 
+        CalendarView calendarView = new CalendarView(); //luo alenterin näkymän
 
+        //Calendar
 
     }
 }
