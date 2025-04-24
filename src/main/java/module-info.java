a891d9628c1d673f16542649a3ad97343371713f
kaisukaisu <kaisu.konttinen@hotmail.com>
2025-04-24 10:52:45 +0300

    kalenterijuttuja ja pom.xml päivityksii, ical ei toimi tho

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,13 @@ module com.example.miniprojekti {
     requires javafx.fxml;
     requires java.sql;
     requires com.calendarfx.view;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome;
+    requires javafx.controls;
+    requires org.controlsfx.controls;
+
+
 
 
     opens com.example.miniprojekti to javafx.fxml;
